71a31c2565c60d07928ef95b8b6889c1d2f1e544
minhuy206 <103843800+minhuy206@users.noreply.github.com>
2023-06-11 13:03:41 +0700

    Update index.jsx

--- src/pages/HomeTemplate/Home/index.jsx
+++ src/pages/HomeTemplate/Home/index.jsx
@@ -10,16 +10,6 @@ function Home() {
   const dispatch = useDispatch();
   const { movies, sliders } = useSelector((state) => state.homeReducer);
   useEffect(() => {
-    localStorage.setItem("User", {
-  accessToken:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoibWluaHV5MjA2IiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvZW1haWxhZGRyZXNzIjoidm9taW5oaHV5MDkxMUBnbWFpbC5jb20iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOlsiS2hhY2hIYW5nIiwidm9taW5oaHV5MDkxMUBnbWFpbC5jb20iLCJHUDAwIl0sIm5iZiI6MTY4NjQ2MzI0NCwiZXhwIjoxNjg2NDY2ODQ0fQ.Vk9lFkflT2B3sKDxGgga78PvQ6MjOocAwgsPo_YnOIs",
-  email: "vominhhuy0911@gmail.com",
-  hoTen: "Huy",
-  maLoaiNguoiDung: "KhachHang",
-  maNhom: "GP00",
-  soDT: "0123456789",
-  taiKhoan: "minhuy206",
-});
     dispatch(fetchMovies());
   }, []);
   const sortMovieByScore = () => {
